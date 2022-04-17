2236db43eb727f48c8607e6f882c1b5361a2226d
Göksel <goksel_koc99@hotmail.com>
2022-04-18 01:10:30 +0300

    toolbar buttons click navigation to login activity added

--- app/src/main/java/com/gokselkoc/greyfood/MainActivity.kt
+++ app/src/main/java/com/gokselkoc/greyfood/MainActivity.kt
@@ -10,13 +10,14 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.gokselkoc.greyfood.databinding.ActivityMainBinding
 import com.gokselkoc.greyfood.ui.BasketActivity
+import com.gokselkoc.greyfood.ui.LoginActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding : ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
     lateinit var navController: NavController
-    lateinit var navHostFragment :NavHostFragment
+    lateinit var navHostFragment: NavHostFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,11 +25,19 @@ class MainActivity : AppCompatActivity() {
         binding.lifecycleOwner = this
 
 
+        bottomMenuNavGraph(binding.mainFragmentNavHost.id, binding.mainBottomMenuNav)
+
         binding.cardView.setOnClickListener {
-            startActivity(Intent(this,BasketActivity::class.java) )
+            startActivity(Intent(this, BasketActivity::class.java))
         }
 
-        bottomMenuNavGraph(binding.mainFragmentNavHost.id, binding.mainBottomMenuNav)
+        binding.toolbarLoginImageView.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+
+        binding.toolbarListImageView.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 
     private fun bottomMenuNavGraph(navHostFragmentId: Int, bottomMenuView: BottomNavigationView) {
